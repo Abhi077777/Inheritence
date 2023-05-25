88f20a2b67edab1cd885e3e306231d5be0158995
Abhi077777 <120783717+Abhi077777@users.noreply.github.com>
2023-05-25 23:14:06 +0530

    Add project files.

---
+++ Inheritence/Inhertence&Encapsulation.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+
+namespace Inheritence
+{
+    class Program
+
+    {
+       
+        static void Main()
+        {
+            //PermaEmployee Abhi = new PermaEmployee();
+            //Abhi.ID = 1;
+            
+
+            //TempEmployee Manu = new TempEmployee();
+            //Manu.ID = 2;
+            //Abhi.Show();
+
+            //Console.WriteLine(Abhi.ID);
+            //Console.WriteLine(Manu.ID);
+        
+            //Console.ReadLine();
+
+
+
+
+            //Person p = new Person();
+            //p.setName("Abhishek Sharma");
+            //p.getName();
+            //p.setAge(26);
+            //p.getAge();
+            
+            //Console.ReadLine();
+
+
+
+
+            Child c = new Child();
+            c.Show();
+            Console.ReadLine();
+
+            ((Parent)c).Show();
+            Console.ReadLine() ;
+
+            Parent z = new Child();
+            z.Show();
+            Console.ReadLine() ;
+
+        }
+    }
+
+    //class TempEmployee : Employee
+    //{
+       
+    //    public int TempEmpSalary;
+    //    public int VisitingHour;
+    //}
+
+    //class PermaEmployee : Employee
+    //{
+    //    public int PermaEmpSalary;
+    //    public int PermanentHour;
+    //}
+    //class Employee
+    //{
+    //    public int ID;
+    //    public string? EmpName;
+    //    public int EmpAge;
+    //    public int EmpContactNo;
+
+    //    public void Show()
+    //    {
+    //        Console.WriteLine("This a Method of Base Class");
+    //    }
+    //}
+
+    class Person 
+    
+    { 
+        private int Age;
+        private string? Name;
+
+        public void setAge(int Age)
+        {
+            if(Age > 0)
+            {
+                this.Age = Age;
+            }
+            else
+            {
+                Console.WriteLine("Age Should not be Negtive or Zero");
+            }
+            
+            
+        }
+        public void getAge()
+        {
+            if(Age > 0)
+            {
+                Console.WriteLine("Your Age is : " + this.Age);
+            }
+            else
+            {
+
+            }
+           
+        } 
+
+        public void setName(string? Name)
+        {
+            if (string.IsNullOrEmpty(Name) == true)
+            {
+                Console.WriteLine("Name is Required");
+            }
+            else
+            {
+                this.Name = Name;
+            }
+            
+        }
+        public void getName()
+        {
+            if(string.IsNullOrEmpty(this.Name) == true)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Your Name is : " + this.Name);
+            }
+            
+        }
+    }
+}
---
+++ Inheritence/Methodhiding.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritence
+{
+    class Methodhiding
+    {
+     
+    }
+    class Parent
+    {
+        public void Show()
+        {
+            Console.WriteLine("This is the method of Parent Class");
+        }
+    }
+    class Child : Parent
+    {
+        public new void  Show()
+        {
+            base.Show();
+            Console.WriteLine("This is the method of Child Class");
+        }
+    }
+}
